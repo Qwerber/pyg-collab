3507580e2e557cbac06a08f25b6e61f56a5e13d8
Oliver Xu <oxu@Olivers-MacBook-Pro.local>
2015-06-23 16:17:50 -0700

    fixed it, your edits were horrible

--- pygc/key.py
+++ pygc/key.py
@@ -4,8 +4,9 @@ class KeyObj:
 	def __init__(self):
 		self.pressed = pygame.key.get_pressed()
 		self.lastPressed = pygame.key.get_pressed()
+	
 	def update(self):
-		self.lastPressed = key.pressed[:]
+		self.lastPressed = self.pressed[:]
 		self.pressed = pygame.key.get_pressed()
 
 	def isPressed(self, _key):
